237386f8ce0b3b39e1ce9a685b2cb0c99b9a1be8
Stann19 <pstanislav2003@gmail.com>
2022-10-22 00:28:27 +0300

    reworked homework

---
+++ 01.10.2022/01.10.2022(1).cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+using namespace std;
+
+struct point {
+    int x;
+    int y;
+};
+
+template <typename T>
+struct Comparator {
+    virtual int operator()(T*, T*) const = 0;
+};
+
+template<>
+struct Comparator<double> {
+    int operator()(double* a, double* b) const {
+        if (*a < *b) {
+            return -1;
+        }
+        if (*a == *b) {
+            return 0;
+        }
+        return 1;
+    }
+};
+
+template<>
+struct Comparator<point> {
+    int operator()(point* a, point* b) const {
+        int square_a = a->x * a->y;
+        int square_b = b->x * b->y;
+        if (square_a < square_b) {
+            return -1;
+        }
+        if (square_a == square_b) {
+            return 0;
+        }
+        return 1;
+    }
+};
+
+void swap(void* lha, void* rha, unsigned int element_byte_size)
+{
+    void* tmp = malloc(element_byte_size);
+    if (tmp == 0) {
+        cout << "error";
+        free(tmp);
+        return;
+    }
+    memcpy(tmp, lha, element_byte_size);
+    memcpy(lha, rha, element_byte_size);
+    memcpy(rha, tmp, element_byte_size);
+    free(tmp);
+}
+
+template <typename T>
+void sort(T* data, int array_size) {
+    Comparator<T> comparator;
+    if (array_size <= 1)
+        return;
+    int element_byte_size = sizeof(T);
+    void* pivot = (char*)data + element_byte_size * (array_size - 1);
+    int f = 1;
+    for (int i = 0; i < array_size - 1; i++)
+    {
+        void* g = (char*)data + element_byte_size * i;
+        if (f == 1 && comparator((T*)pivot, (T*)g) == -1)
+        {
+            swap(g, pivot, element_byte_size);
+            pivot = g;
+            f = -1;
+        }
+        else if (f == -1 && comparator((T*)g, (T*)pivot) == -1)
+        {
+            while (pivot != g)
+            {
+                swap((char*)g - element_byte_size, g, element_byte_size);
+                g = (char*)g - element_byte_size;
+            }
+            pivot = (char*)pivot + element_byte_size;
+        }
+    }
+    sort(data, ((char*)pivot - (void*)(char*)data) / element_byte_size);
+    sort((T*)((char*)pivot + element_byte_size), array_size - 1 - ((char*)pivot - (void*)(char*)data) / element_byte_size);
+}
+
+int main()
+{
+    cout << "If you want to sort type point enter 0" << " " << "If you want to sort type double enter 1" << "\n";
+    int n;
+    cin >> n;
+    if (n == 0) {
+        int n_point;
+        cin >> n_point;
+        point* a = new point[n_point];
+        for (int i = 0; i < n_point; i++) {
+            cin >> a[i].x;
+            cin >> a[i].y;
+        }
+        sort<point>(a, n_point);
+        for (int i = 0; i < n_point; i++) {
+            cout << a[i].x << " " << a[i].y << endl;
+        }
+        delete[] a;
+        return 0;
+    }
+
+    if (n == 1) {
+        int n_double;
+        cin >> n_double;
+        double* b = new double[n_double];
+        for (int i = 0; i < n_double; i++) {
+            cin >> b[i];
+        }
+        sort<double>(b, n_double);
+        for (int i = 0; i < n_double; i++) {
+            cout << b[i] << endl;
+        }
+        delete[] b;
+        return 0;
+    }
+}
---
+++ 01.10.2022/01.10.2022(2).cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+using namespace std;
+
+struct point {
+    double x;
+    double y;
+};
+
+template <typename T>
+struct Comparator {
+    virtual int operator()(T&, T&) const = 0;
+};
+
+template <>
+struct Comparator<double> {
+    int operator()(double& a, double& b) const {
+        if (a < b) {
+            return -1;
+        }
+        if (a == b) {
+            return 0;
+        }
+        return 1;
+    }
+};
+
+template<>
+struct Comparator<point> {
+    int operator()(point& a, point& b) const {
+        double square_a = a.x * a.y;
+        double square_b = b.x * b.y;
+        if (square_a < square_b) {
+            return -1;
+        }
+        if (square_a == square_b) {
+            return 0;
+        }
+        return 1;
+    }
+};
+
+template <typename T, typename P>
+class PriorityQueue {
+private:
+    T* data;
+    P* prior;
+    int size;
+    bool emptyness;
+    Comparator<T>& comparator_data;
+    Comparator<P>& comparator_prior;
+
+public:
+    PriorityQueue(Comparator<T>& comparator_data, Comparator<P>& comparator_prior) : comparator_data(comparator_data), comparator_prior(comparator_prior), emptyness(true), size(0) {
+        P* b = new P[size];
+        T* a = new T[size];
+        data = a;
+        prior = b;
+    }
+
+    void print() {
+        using namespace std;
+        cout << "[";
+        for (int i = 0; i < size; i++) {
+            cout << "(" << data[i] << ";" << prior[i] << ")";
+
+        }
+        cout << "]" << endl;
+    }
+
+    template<typename F>
+    void swap(F* t, int i, int j) {
+        F temp;
+        temp = t[j];
+        t[j] = t[i];
+        t[i] = temp;
+        return;
+    }
+
+    void swap_with_ancestor(int i) {
+        if (i == 0) {
+            return;
+        }
+
+        if ((comparator_prior(prior[i], prior[(i - 1) / 2]) == 1) || (comparator_prior(prior[i], prior[(i - 1) / 2]) == 0 && comparator_data(data[i], data[(i - 1) / 2]) == 1)) {
+            swap<T>(data, i, (i - 1) / 2);
+            swap<P>(prior, i, (i - 1) / 2);
+            swap_with_ancestor((i - 1) / 2);
+        }
+        return;
+    }
+
+    void push(T to_push, P priority) {
+        size++;
+        T* a = new T[size];
+        for (int i = 0; i < size - 1; i++) {
+            a[i] = data[i];
+        }
+        a[size - 1] = to_push;
+        data = a;
+
+        P* b = new P[size];
+        for (int i = 0; i < size - 1; i++) {
+            b[i] = prior[i];
+        }
+        b[size - 1] = priority;
+        prior = b;
+
+        swap_with_ancestor(size - 1);
+        return;
+    }
+
+    void peek() {
+        cout << "(" << data[0] << ";" << prior[0] << ")" << endl;
+    }
+
+    void swap_with_descendant(int i) {
+
+        if (2 * i + 1 >= size) {
+            return;
+        }
+        if ((comparator_prior(prior[i], prior[2 * i + 1]) == -1) || (comparator_prior(prior[i], prior[2 * i + 1]) == 0 && comparator_data(data[i], data[2 * i + 1]) == -1)) {
+            swap<T>(data, i, 2 * i + 1);
+            swap<P>(prior, i, 2 * i + 1);
+            swap_with_descendant(2 * i + 1);
+        }
+        if (2 * i + 2 < size) {
+            if ((comparator_prior(prior[i], prior[2 * i + 2]) == -1) || (comparator_prior(prior[i], prior[2 * i + 2]) == 0 && comparator_data(data[i], data[2 * i + 2]) == -1)) {
+                swap<T>(data, i, 2 * i + 2);
+                swap<P>(prior, i, 2 * i + 2);
+                swap_with_descendant(2 * i + 2);
+            }
+        }
+        return;
+    }
+
+    void poll() {
+        if (size == 0) {
+            cout << "heap is empty" << endl;
+            return;
+        }
+        size--;
+        T* a = new T[size];
+        for (int i = 1; i < size; i++) {
+            a[i] = data[i];
+        }
+        a[0] = data[size];
+        data = a;
+
+        P* b = new P[size];
+        for (int i = 1; i < size; i++) {
+            b[i] = prior[i];
+        }
+        b[0] = prior[size];
+        prior = b;
+
+        swap_with_descendant(0);
+        return;
+    }
+
+    bool is_empty() {
+        return size == 0;
+    }
+
+    void free() {
+        delete[] data;
+        delete[] prior;
+        return;
+    }
+};
+
+int main()
+{
+    Comparator<double>comparator_data;
+    Comparator<double>comparator_prior;
+    PriorityQueue<double, double> p(comparator_data, comparator_prior);
+    p.is_empty();
+    p.push(2, 1);
+    p.push(3, 2);
+    p.push(4, 1);
+    p.push(8, 1);
+    p.print();
+    p.peek();
+    p.print();
+    p.poll();
+    p.print();
+    p.free();
+    return 0;
+}
